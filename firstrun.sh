80676bb486fee4e07030c386446e596ca6b90765
root <root@batz.home.local>
2017-12-08 08:21:33 -0600

    commit

---
+++ firstrun.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# install packages
+rpm -q epel-release || yum install epel-release -y
+rpm -q git || yum install git -y
+rpm -q ansible || yum install ansible -y
+rpm -q libselinux-python || yum install libselinux-python -y
+
+# configure
+cd /tmp
+git clone http://github.com/jlim0930/mine/mine.git
+cd mine
+ansible-playbook -i "localhost," -c local local.yml
+
